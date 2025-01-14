a203f10582aca680bbeb08a23076f30dd2d5fb2c
Mahmoud Afarideh <mahmoudafarideh@gmail.com>
2025-01-14 12:12:25 +0330

    Fixes poet details scroll

--- app/src/main/java/m/a/poem/ui/poet/screen/PoetScreen.kt
+++ app/src/main/java/m/a/poem/ui/poet/screen/PoetScreen.kt
@@ -5,6 +5,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -96,6 +98,7 @@ fun PoetScreen(
                             style = MaterialTheme.typography.bodyMedium,
                             modifier = Modifier
                                 .padding(horizontal = 24.dp)
+                                .verticalScroll(rememberScrollState())
                                 .padding(
                                     horizontal =
                                         when (windowSize.widthSizeClass) {
